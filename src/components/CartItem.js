79eeb6903886611e8324d8629092a1e385f2874a
Ranjeet Kumar <ranjeet.kumar.23.2@mountblue.tech>
2023-04-24 15:51:18 +0530

    Changed button ui and remove prompt message in cart page

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -3,126 +3,124 @@ import { connect } from "react-redux";
 import { DECREMENT, INCREMENT, REMOVE_FROM_CART } from "../redux/actionTypes";
 
 class CartItem extends Component {
-
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
-            removeItem : false,
-        }
+            removeItem: false,
+        };
     }
 
     handleRemovePrompt = () => {
-        this.setState(({
-            removeItem : true,
-        }));
-    }
+        this.setState({
+            removeItem: true,
+        });
+    };
 
     handleYes = () => {
         this.props.removeFromCart(this.props.item.id);
-    }
-    
+    };
+
     handleNo = () => {
-        this.setState(({
-            removeItem : false,
-        }));
-    }
+        this.setState({
+            removeItem: false,
+        });
+    };
 
     handlePlusClick = () => {
         this.props.incrementQuantity(this.props.item.id);
-    }
+    };
 
     handleMinusClick = () => {
         this.props.decrementQuantity(this.props.item.id);
-    }
+    };
 
     render() {
-
-        const { title, price, image, quantity} = this.props.item;
-        const { removeItem} = this.state;
+        const { title, price, image, quantity } = this.props.item;
+        const { removeItem } = this.state;
         return (
-            <>
-                {!removeItem && (
-                    <div className="cart_item">
-                        <img className="cart_img" src={image} alt={title}></img>
-                        <div className="des">
-                            <h4>{title}</h4>
-                            <div className="add_more">
-                                <button
-                                    className="btns"
-                                    disabled={ quantity === 1? true : false}
-                                    onClick={this.handleMinusClick}
-                                >
-                                    -
-                                </button>
-                                <span>{quantity}</span>
-                                <button
-                                    className="btns"
-                                    disabled={ quantity === 5? true : false}
-                                    onClick={this.handlePlusClick}
-                                >
-                                    +
-                                </button>
-                            </div>
-                            <div className="price_div">
-                                Price:
-                                <span className="price">
-                                    <span className="dollar">$</span>
-                                    {(price * quantity).toFixed(2)}
-                                </span>
-                            </div>
+            <div className="cart_item">
+                <img className="cart_img" src={image} alt={title}></img>
+                <div className="des">
+                    <h4>{title}</h4>
+                    <div className="add_more">
+                        <button
+                            className="btns"
+                            disabled={quantity === 1 ? true : false}
+                            onClick={this.handleMinusClick}
+                        >
+                            -
+                        </button>
+
+                        <span>{quantity}</span>
+                        
+                        <button
+                            className="btns"
+                            disabled={quantity === 5 ? true : false}
+                            onClick={this.handlePlusClick}
+                        >
+                            +
+                        </button>
+                    </div>
+                    <div className="price_div">
+                        Price:
+                        <span className="price">
+                            <span className="dollar">$</span>
+                            {(price * quantity).toFixed(2)}
+                        </span>
+                    </div>
+                    <div>
+                        {
+                            !removeItem && 
                             <button
-                                className="remove_btn"
+                                className="btn btn-danger"
                                 onClick={this.handleRemovePrompt}
                             >
                                 Remove Item
                             </button>
-                        </div>
+                        }
+                        {
+                            removeItem &&
+                            <>
+                                <small className="me-1">Remove Item ?</small>
+                                <button className="btn btn-outline-danger me-3" onClick={this.handleYes}>
+                                    Yes
+                                </button>
+                                <button className="btn btn-outline-success me-3" onClick={this.handleNo}>
+                                    No
+                                </button>
+                            </>
+                        }
                     </div>
-                )}
-                {removeItem && (
-                    <div className="remove_item cart_item">
-                        <h4>Are you sure you want to remove?</h4>
-                        <div className="buttons">
-                            <button className="yes_btn" onClick={this.handleYes}>
-                                Yes
-                            </button>
-                            <button className="no_btn" onClick={this.handleNo}>
-                                No
-                            </button>
-                        </div>
-                    </div>
-                )}
-            </>
+                </div>
+            </div>
         );
     }
 }
 
-function mapDispatchToProps(dispatch){
-
+function mapDispatchToProps(dispatch) {
     return {
-
         removeFromCart: (removeItemId) => {
             dispatch({
                 type: REMOVE_FROM_CART,
-                payload : removeItemId,
-            })
+                payload: removeItemId,
+            });
         },
 
-        incrementQuantity : (itemId) => {
+        incrementQuantity: (itemId) => {
             dispatch({
                 type: INCREMENT,
                 payload: itemId,
-            })
+            });
         },
 
-        decrementQuantity : (itemId) => {
+        decrementQuantity: (itemId) => {
             dispatch({
                 type: DECREMENT,
                 payload: itemId,
-            })
-        }
-    }
+            });
+        },
+    };
 }
 
 export default connect(null, mapDispatchToProps)(CartItem);
